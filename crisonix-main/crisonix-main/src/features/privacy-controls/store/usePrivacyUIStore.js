af586994d201d2e4da5e8910bf7de71079fd3641
Abbas1709 <166807334+Abbas1709@users.noreply.github.com>
2026-06-23 12:53:54 +0530

    Delete crisonix-main directory

--- crisonix-main/crisonix-main/src/features/privacy-controls/store/usePrivacyUIStore.js
+++
@@ -1,7 +0,0 @@
-import { create } from 'zustand';
-
-export const usePrivacyUIStore = create((set) => ({
-    activeMenu: null, // 'profilePhoto', 'about', 'profilePhotoExceptions', 'aboutExceptions'
-    setActiveMenu: (menu) => set({ activeMenu: menu }),
-    closeMenu: () => set({ activeMenu: null })
-}));
